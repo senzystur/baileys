0920c2e072256f164cf7f9f743fc6623482bbb08
senzystur <senzyyuotube@gmail.com>
2026-01-17 09:53:42 +0700

    Add files via upload

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,12 +1,3 @@
-import makeWASocket from './Socket';
-export * from '../WAProto';
-export * from './Utils';
-export * from './Types';
-export * from './Store';
-export * from './Defaults';
-export * from './WABinary';
-export * from './WAM';
-export * from './WAUSync';
-export type WASocket = ReturnType<typeof makeWASocket>;
-export { makeWASocket };
-export default makeWASocket;
+export * from './constants';
+export * from './encode';
+export * from './BinaryInfo';
